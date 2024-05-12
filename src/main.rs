c4d532545b2217c327b17a9535d3b224ccba5bd2
Rishabh Chawla <rishabh31121999@gmail.com>
2024-05-13 00:14:02 +0530

    chore: echo server

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
-fn respond_with_header(header: &str, mut stream: &TcpStream) {
-    let response = format!("{header}\r\n\r\n");
+fn respond_stream(response: &str, mut stream: &TcpStream) {
     stream.write_all(response.as_bytes()).unwrap()
 }
 
@@ -21,19 +20,26 @@ fn read_request(mut stream: &TcpStream) -> Vec<String> {
 
 fn process_request(stream: &TcpStream) {
     let response = read_request(stream);
-    let request_line = response[0].clone();
+    let status_line = response[0].clone();
 
-    if request_line == "GET / HTTP/1.1" {
-        return respond_with_header("HTTP/1.1 200 OK", stream);
+    let status_parts: Vec<&str> = status_line.split(" ").collect();
+    let path_fragments: Vec<&str> = status_parts[1].split("/").collect();
+
+    if status_parts[0] == "GET" && path_fragments[1] == "echo" && path_fragments.len() >= 3 {
+        let string = path_fragments[2];
+        let length = string.len();
+        return respond_stream(
+            format!("HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-length: {length}\r\n\r\n{string}").as_str(),
+            stream
+        );
+    } else if status_parts[0] == "GET" && status_parts[1] == "/" {
+        return respond_stream("HTTP/1.1 200 OK\r\n\r\n", stream);
     }
 
-    respond_with_header("HTTP/1.1 404 Not Found", stream)
+    respond_stream("HTTP/1.1 404 Not Found\r\n\r\n", stream)
 }
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    // println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
